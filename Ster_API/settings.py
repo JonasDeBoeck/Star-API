006ca1874f5df4bb3a8b0ad874bc2732ce305ce4
JonasDeBoeck <Jonas.deboeck@student.ucll.be>
2020-11-29 19:54:46 +0100

    DB change

--- Ster_API/settings.py
+++ Ster_API/settings.py
@@ -51,7 +51,6 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,6 +146,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
