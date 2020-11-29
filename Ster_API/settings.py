2b5d9a2a2e118fa8142ea81400d8afb840e8b9f5
JonasDeBoeck <Jonas.deboeck@student.ucll.be>
2020-11-29 19:41:55 +0100

    Changed database configuration

--- Ster_API/settings.py
+++ Ster_API/settings.py
@@ -131,3 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
